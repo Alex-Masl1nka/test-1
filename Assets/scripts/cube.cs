6264145a888c7244141b51ae4a036498dfb9b1d1
Alex <alexaf_2004@mail.ru>
2020-12-03 19:37:27 +0300

    commit

---
+++ Assets/scripts/cube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    public int a = 1;
+    public int b = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // сложение
+        //a = a + 1;
+        //a += 1;
+        //a++;
+        print("сумма a и b " + (a+b));
+
+
+        // вычитание
+        //b = b - 1;
+        //b -= 1;
+        //b--;
+        print("разность a и b " + (a - b));
+
+        // умножение
+        //a = a * 1;
+        //a *= 1;
+        print("произведение a и b " + (a * b));
+
+        // деление
+        //a = a / 1;
+        //a /= 1;
+        print("деление a и b " + (float)a / b);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/test1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject example;
+    public Transform SpawnPoint;
+    void Start()
+    {
+        
+    }
+    
+    //void Inst()
+    //{
+    //    cube = Instantiate(example, SpawnPoint.position, SpawnPoint.rotation, transform);
+    //    cube.GetComponent<MeshRenderer>().material.color = Color.green;
+    //}
+    void Update()
+    {
+        sphere = Instantiate(example, SpawnPoint.position, SpawnPoint.rotation, transform);
+        sphere.GetComponent<MeshRenderer>().material.color = Color.green;
+        Rigidbody rig = sphere.GetComponent<Rigidbody>();
+        rig.useGravity = true;
+
+    }
+    //void Destr()
+    //{
+    //    Destroy(cube, 2);
+    //}
+    //void EnDisable()
+    //{
+    //    cube.SetActive(!(cube.activeSelf));
+    //}
+    //void ObjAndComp()
+    //{
+    //    cube.GetComponent<MeshRenderer>().material.color = Color.green;
+    //}
+}
